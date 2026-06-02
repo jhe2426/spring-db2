05bded1de22054bbbf5ee2e1a2a502edbb7f5b5a
jhe2426 <jhe2426@naver.com>
2026-06-02 20:18:51 +0900

    JdbcTemplate 적용1 - 기본

---
+++ itemservice-db/src/main/java/hello/itemservice/repository/jdbctemplate/JdbcTemplateItemRepositoryV1.java
@@ -0,0 +1,133 @@
+package hello.itemservice.repository.jdbctemplate;
+
+import hello.itemservice.domain.Item;
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.ItemSearchCond;
+import hello.itemservice.repository.ItemUpdateDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.util.StringUtils;
+
+import javax.sql.DataSource;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+/*
+    JdbcTemplate
+*/
+@Slf4j
+public class JdbcTemplateItemRepositoryV1 implements ItemRepository {
+
+    private final JdbcTemplate template;
+
+    public JdbcTemplateItemRepositoryV1(DataSource dataSource) {
+        this.template = new JdbcTemplate(dataSource);
+    }
+
+    @Override
+    public Item save(Item item) {
+        String sql = "insert into item(item_name, price, quantity) values (?,?,?)";
+
+        /*
+            데이터를 저장할 때 PK 생성 옵션이 auto increment 방식을 사용하기 때문에, PK인 ID 값을 개발자가 직접
+            지정하는 것이 아니라 비워두고 저장해야 함 문제는 이렇게 데이터베이스가 대신 생성해주는 PK ID 값은 데이터베이스가
+            생성하기 때문에, 데이터베이스에 INSERT가 완료 되어야 생성된 PK ID 값을 확인할 수 있다.
+
+            KeyHolder와 connection.prepareStatement(sql, new String[]{"id})를 사용해서 Id를 지정해주면
+            INSERT 쿼리 실행 이후에 데이터베이스에서 생성된 ID 값을 조회할 수 있다.
+        */
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        /*
+            template.update(): 데이터를 변경할 때는 update()를 사용하면 됨
+                INSERT, UPDATE, DELETE SQL에 사용
+                반환 값은 int인데, 영향 받은 로우 수를 반환
+        */
+        template.update(connection -> {
+            // 자동 증가 키
+            PreparedStatement ps = connection.prepareStatement(sql, new String[]{"id"});
+            ps.setString(1, item.getItemName());
+            ps.setInt(2, item.getPrice());
+            ps.setInt(3, item.getQuantity());
+            return ps;
+        }, keyHolder);
+
+        long key = keyHolder.getKey().longValue();
+        item.setId(key);
+
+        return item;
+    }
+
+    @Override
+    public void update(Long itemId, ItemUpdateDto updateParam) {
+        String sql = "update item set item_name=?, price=?, quantity=?, where id=?";
+        template.update(sql,
+                updateParam.getItemName(),
+                updateParam.getPrice(),
+                updateParam.getQuantity(),
+                itemId);
+
+    }
+
+    @Override
+    public Optional<Item> findById(Long id) {
+        String sql = "select id, item_name, price, quantity from item where id = ?";
+        try {
+            Item item = template.queryForObject(sql, itemRowMapper(), id);
+            return Optional.of(item);
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public List<Item> findAll(ItemSearchCond cond) {
+        String itemName = cond.getItemName();
+        Integer maxPrice = cond.getMaxPrice();
+
+        String sql = "select id, item_name, price, quantity from item";
+        // 동적 쿼리
+        if (StringUtils.hasText(itemName) || maxPrice != null) {
+            sql += " where";
+        }
+
+        boolean andFlag = false;
+        List<Object> param = new ArrayList<>();
+        if (StringUtils.hasText(itemName)) {
+            sql += " item_name like concat('%',?,'%')";
+            param.add(itemName);
+            andFlag = true;
+        }
+
+        if (maxPrice != null) {
+            if (andFlag) {
+                sql += " and";
+            }
+            sql += " price <= ?";
+            param.add(maxPrice);
+        }
+
+        log.info("sql={}", sql);
+
+        return template.query(sql, itemRowMapper(), param.toArray());
+    }
+
+    private RowMapper<Item> itemRowMapper() {
+        return ((rs, rowNum) -> {
+            Item item = new Item();
+            item.setId(rs.getLong("id"));
+            item.setItemName(rs.getString("item_name"));
+            item.setPrice(rs.getInt("price"));
+            item.setQuantity(rs.getInt("quantity"));
+            return item;
+        });
+    }
+
+}
