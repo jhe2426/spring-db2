4f025e7459d8bd95e71a76b1e12e472d9fcf5311
jhe2426 <jhe2426@naver.com>
2026-06-20 22:49:58 +0900

    트랜잭션 적용 위치

---
+++ springtx/src/test/java/hello/springtx/apply/TxLevelTest.java
@@ -0,0 +1,55 @@
+package hello.springtx.apply;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+@SpringBootTest
+public class TxLevelTest {
+
+    @Autowired LevelService service;
+
+    @Test
+    void orderTest() {
+        service.write();
+        service.read();
+    }
+
+    @TestConfiguration
+    static class TxLevelTestConfig {
+        @Bean
+        LevelService levelService() {
+            return new LevelService();
+        }
+    }
+
+    @Slf4j
+    @Transactional(readOnly = true) // readOnly = true는 트랜잭션이 읽기 전용으로만 만들어지도록 하는 옵션
+    static class LevelService {
+
+        // @Transactional 애노테이션의 reaOnly옵션의 기본 설정 값이 false이므로 생략이 가능함
+        @Transactional(readOnly = false)
+        public void write() {
+            log.info("call write");
+            printTxInfo();
+        }
+
+        public void read() {
+            log.info("call read");
+            printTxInfo();
+        }
+
+        private void printTxInfo() {
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active={} ", txActive);
+            boolean readOnly = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
+            log.info("tx readOnly={} ", readOnly);
+        }
+    }
+
+}
